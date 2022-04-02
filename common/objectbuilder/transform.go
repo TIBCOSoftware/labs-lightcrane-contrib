cde4cbf05932bbca51bf253567391fcb73d337c7
Infospecktra <Infospecktra@gmail.com>
2022-04-02 15:24:38 -0400

    Refactor
    Extract common module from parameter builders

---
+++ common/objectbuilder/transform.go
@@ -0,0 +1,207 @@
+/*
+ * Copyright © 2020. TIBCO Software Inc.
+ * This file is subject to the license terms contained
+ * in the license file that is distributed with this file.
+ */
+package objectbuilder
+
+import (
+	"fmt"
+	"strings"
+
+	kwr "github.com/TIBCOSoftware/labs-lightcrane-contrib/common/keywordreplace"
+	"github.com/TIBCOSoftware/labs-lightcrane-contrib/common/util"
+)
+
+func CreateDockerF1Properties(
+	pathMapper *kwr.KeywordMapper,
+	defVariable map[string]interface{},
+	propertyPrefix string,
+	appProperties []interface{},
+	gProperties []map[string]interface{},
+	ports []interface{},
+) (interface{}, error) {
+
+	description := make([]interface{}, 0)
+	mainDescription := map[string]interface{}{
+		"Group": "main",
+		"Value": make([]interface{}, 0),
+	}
+	description = append(description, mainDescription)
+
+	for _, property := range gProperties {
+		/* nil will bot be accepted */
+		value, dtype, err := util.GetPropertyValue(property["Value"], property["Type"])
+		if nil != err {
+			return nil, err
+		}
+		log.Debug("[createDockerF1Properties] Name = ", property["Name"], ", Raw Value = ", value, ", defVariable = ", defVariable)
+
+		if "String" == dtype {
+			value = pathMapper.Replace(value.(string), defVariable)
+			log.Debug("[createDockerF1Properties] Value after replace = ", value)
+			sValue := value.(string)
+			if "" != sValue && sValue[0] == '$' && sValue[len(sValue)-1] == '$' {
+				continue
+			}
+		}
+		mainDescription["Value"] = append(mainDescription["Value"].([]interface{}), map[string]interface{}{
+			"Name":  pathMapper.Replace(util.GetPropertyElementAsString("Name", property), defVariable),
+			"Value": value,
+			"Type":  util.GetPropertyElementAsString("Type", property),
+		})
+	}
+	for index, property := range appProperties {
+		mainDescription["Value"] = append(mainDescription["Value"].([]interface{}), map[string]interface{}{
+			"Name":  pathMapper.Replace(fmt.Sprintf("%s.environment[%d]", propertyPrefix, index), defVariable),
+			"Value": fmt.Sprintf("%s=%s", util.GetPropertyElement("Name", property), util.GetPropertyElement("Value", property)),
+			"Type":  util.GetPropertyElement("Type", property),
+		})
+	}
+	index := 0
+	for _, port := range ports {
+		mainDescription["Value"] = append(mainDescription["Value"].([]interface{}), map[string]interface{}{
+			"Name":  pathMapper.Replace(fmt.Sprintf("%s.ports[%d]", propertyPrefix, index), defVariable),
+			"Value": port,
+			"Type":  "String",
+		})
+		index++
+	}
+	return description, nil
+}
+
+func CreateK8sF1Properties(
+	pathMapper *kwr.KeywordMapper,
+	defVariable map[string]interface{},
+	propertyPrefix string,
+	appProperties []interface{},
+	gProperties []map[string]interface{},
+	ports []interface{},
+) (interface{}, error) {
+	groupProperties := make(map[string]interface{})
+	for _, property := range gProperties {
+		name := util.GetPropertyElementAsString("Name", property)
+		group := name[0:strings.Index(name, "_")]
+		if nil == groupProperties[group] {
+			groupProperties[group] = make([]interface{}, 0)
+		}
+		name = name[strings.Index(name, "_")+1 : len(name)]
+		property["Name"] = name
+		groupProperties[group] = append(groupProperties[group].([]interface{}), property)
+	}
+	/*
+		{
+			"Group":"main",
+			"Value":[
+				{"Name":"apiVersion","Type":null,"Value":"apps/v1"},
+				{"Name":"kind","Type":null,"Value":"Deployment"},
+				{"Name":"metadata.name","Type":null,"Value":"http_dummy"},
+				{"Name":"spec.template.spec.containers[0].image","Type":null,"Value":"bigoyang/http_dummy:0.2.1"},
+				{"Name":"spec.template.spec.containers[0].name","Type":null,"Value":"http_dummy"},
+				{"Name":"spec.selector.matchLabels.component","Type":null,"Value":"http_dummy"},
+				{"Name":"spec.template.metadata.labels.component","Type":null,"Value":"http_dummy"},
+				{"Name":"spec.template.spec.containers[0].env[0].name","Type":"string","Value":"Logging_LogLevel"},
+				{"Name":"spec.template.spec.containers[0].env[0].value","Type":null,"Value":"INFO"},
+				{"Name":"spec.template.spec.containers[0].env[1].name","Type":"string","Value":"FLOGO_APP_PROPS_ENV"},
+				{"Name":"spec.template.spec.containers[0].env[1].value","Type":null,"Value":"auto"},
+				{"Name":"spec.template.spec.containers[0].ports[0]","Type":"String","Value":"9999"}
+			]
+		},
+	*/
+	description := make([]interface{}, 0)
+	mainDescription := map[string]interface{}{
+		"Group": "main",
+		"Value": make([]interface{}, 0),
+	}
+	description = append(description, mainDescription)
+
+	for _, iProperty := range groupProperties["main"].([]interface{}) {
+		property := iProperty.(map[string]interface{})
+		value, dtype, err := util.GetPropertyValue(property["Value"], property["Type"])
+		if nil != err {
+			return nil, err
+		}
+		if "String" == dtype {
+			value = pathMapper.Replace(value.(string), defVariable)
+		}
+		mainDescription["Value"] = append(mainDescription["Value"].([]interface{}), map[string]interface{}{
+			"Name":  pathMapper.Replace(util.GetPropertyElementAsString("Name", property), defVariable),
+			"Value": value,
+			"Type":  util.GetPropertyElement("Type", property),
+		})
+	}
+	for index, property := range appProperties {
+		mainDescription["Value"] = append(mainDescription["Value"].([]interface{}), map[string]interface{}{
+			"Name":  pathMapper.Replace(fmt.Sprintf("%s.env[%d].name", propertyPrefix, index), defVariable),
+			"Value": util.GetPropertyElement("Name", property),
+			"Type":  "string",
+		})
+		mainDescription["Value"] = append(mainDescription["Value"].([]interface{}), map[string]interface{}{
+			"Name":  pathMapper.Replace(fmt.Sprintf("%s.env[%d].value", propertyPrefix, index), defVariable),
+			"Value": util.GetPropertyElement("Value", property),
+			"Type":  util.GetPropertyElement("Type", property),
+		})
+	}
+
+	if nil != ports && 0 < len(ports) {
+		ipServiceDescription := map[string]interface{}{
+			"Group": "ip-service",
+			"Value": make([]interface{}, 0),
+		}
+		description = append(description, ipServiceDescription)
+
+		/*
+			{
+				"Group":"ip-service",
+				"Value":[
+					{"Name":"apiVersion","Type":"String","Value":"v1"},
+					{"Name":"kind","Type":"String","Value":"Service"},
+					{"Name":"metadata.name","Type":"String","Value":"$name$-ip-service"},
+					{"Name":"spec.selector.component","Type":"String","Value":"$name$"},
+					{"Name":"spec.type","Type":"String","Value":"LoadBalancer"},
+					{"Name":"spec.port[0]","Type":"String","Value":"8080"},
+					{"Name":"spec.targetPort[0]","Type":"String","Value":"9999"}
+				]
+			}
+		*/
+		for _, iProperty := range groupProperties["ip-service"].([]interface{}) {
+			property := iProperty.(map[string]interface{})
+			value, dtype, err := util.GetPropertyValue(property["Value"], property["Type"])
+			if nil != err {
+				return nil, err
+			}
+			if "String" == dtype {
+				value = pathMapper.Replace(value.(string), defVariable)
+			}
+			ipServiceDescription["Value"] = append(ipServiceDescription["Value"].([]interface{}), map[string]interface{}{
+				"Name":  pathMapper.Replace(util.GetPropertyElementAsString("Name", property), defVariable),
+				"Value": value,
+				"Type":  util.GetPropertyElement("Type", property),
+			})
+		}
+
+		index := 0
+		for _, port := range ports {
+			portPair := strings.Split(port.(string), ":")
+			mainDescription["Value"] = append(mainDescription["Value"].([]interface{}), map[string]interface{}{
+				"Name":  pathMapper.Replace(fmt.Sprintf("%s.ports[%d]", propertyPrefix, index), defVariable),
+				"Value": portPair[1],
+				"Type":  "String",
+			})
+
+			ipServiceDescription["Value"] = append(ipServiceDescription["Value"].([]interface{}), map[string]interface{}{
+				"Name":  fmt.Sprintf("spec.ports[%d].port", index),
+				"Value": portPair[0],
+				"Type":  "String",
+			})
+			ipServiceDescription["Value"] = append(ipServiceDescription["Value"].([]interface{}), map[string]interface{}{
+				"Name":  fmt.Sprintf("spec.ports[%d].targetPort", index),
+				"Value": portPair[1],
+				"Type":  "String",
+			})
+			index++
+		}
+	}
+
+	return description, nil
+}
